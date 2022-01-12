cb2216a675c4ccccb4a0fb0dd29e7d98ed68331c
ferrucci-je <ferrucci.je@gmail.com>
2022-01-11 23:57:50 -0300

    Segunda Entrega Navbar

---
+++ src/NavBar.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+export const NavBar = () => {
+    return (
+    <ul className="navbar">
+        <li>Sección I</li>
+        <li>Sección II</li>
+        <li>Sección III</li>
+        <li>Sección IV</li>
+    </ul>
+    )
+}
+
